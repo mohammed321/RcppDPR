de9583ad064a6925c9b60f5cce1841c6a075da01
mohammad abu gazala <abugazalamohammad@gmail.com>
2025-02-04 11:37:05 -0800

    debug

--- tests/testthat/test-output_change.R
+++ tests/testthat/test-output_change.R
@@ -1,12 +1,1 @@
 
-test_that("testing if gibbs_without_u_screen result changed", {
-  expect_snapshot_value(c(dpr_model_gibbs$alpha, dpr_model_gibbs$beta), style = "serialize")
-})
-
-test_that("testing if VB result changed", {
-  expect_snapshot_value(c(dpr_model_vb$alpha, dpr_model_vb$beta), style = "serialize")
-})
-
-test_that("testing if adaptive_gibbs_without_u_screen result changed", {
-  expect_snapshot_value(c(dpr_model_adaptive_gibbs$alpha, dpr_model_adaptive_gibbs$beta), style = "serialize")
-})
